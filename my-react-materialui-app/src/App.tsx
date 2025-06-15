9e712d171fe2218c432c9f257ba0bdf112075b7c
Rohith Pavithran <rohithtp@gmail.com>
2025-06-15 09:35:16 +0000

    UI: enhance landing page and TreeTablePage with improved Material UI aesthetics

--- my-react-materialui-app/src/App.tsx
+++ my-react-materialui-app/src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, Box, Typography, Button, Container, Paper, Divider } from '@mui/material';
 import theme from './theme/theme';
 import ExampleComponent from './components/ExampleComponent';
 import TreeTablePage from './components/TreeTablePage';
 import { ThemeProvider } from '@mui/material/styles';
 
 const LandingPage: React.FC = () => (
-  <div>
-    <h1>Welcome to My React Material UI App</h1>
-    <ExampleComponent />
-    <Link to="/tree-table">Go to Tree Table Page</Link>
-  </div>
+  <Container maxWidth="md" sx={{ mt: 8 }}>
+    <Paper elevation={4} sx={{ p: 4, borderRadius: 3, textAlign: 'center' }}>
+      <Typography variant="h3" color="primary" gutterBottom>
+        Welcome to My React Material UI App
+      </Typography>
+      <Divider sx={{ mb: 3 }} />
+      <Box mb={3}>
+        <ExampleComponent />
+      </Box>
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        component={Link}
+        to="/tree-table"
+        sx={{ mt: 2, borderRadius: 2, fontWeight: 600 }}
+      >
+        Go to Tree Table Page
+      </Button>
+    </Paper>
+  </Container>
 );
 
 const App: React.FC = () => {
